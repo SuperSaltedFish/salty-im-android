ede67131609ed08992fc82ae93f89c85c063a0bb
zhixingye <zhixingye@tencent.com>
2019-12-31 17:45:43 +0800

    添加im模块

--- app/src/main/java/me/zhixingye/salty/MainActivity.java
+++ app/src/main/java/me/zhixingye/salty/MainActivity.java
@@ -1,8 +1,20 @@
 package me.zhixingye.salty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +22,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Button b = findViewById(R.id.mBtn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        onclick(null);
+    }
+
+    public void onclick(View v) {
+//        new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/SaltyIM/").mkdirs();
+        String file = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SaltyIM/aaaaa.apk";
+//        File file1 = new File(file);
+//        try {
+//            if (!file1.exists() && !file1.createNewFile()) {
+//                return;
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return;
+//        }
+        startActivity(getPendingIntent(file));
+
+    }
+
+    private Intent getPendingIntent(String filePath) {
+        Intent installIntent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = null;
+        int targetSDK = getApplicationInfo().targetSdkVersion;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && targetSDK >= Build.VERSION_CODES.N) {
+            uri = FileProvider.getUriForFile(this, getPackageName(), new File(filePath));
+            installIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            installIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+        if (uri == null) {
+            uri = Uri.fromFile(new File(filePath));
+        }
+        installIntent.setPackage(getPackageName());
+        installIntent.setDataAndType(uri, "application/vnd.android.package-archive");
+        installIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return installIntent;
     }
 }
---
+++ lib_im/src/test/java/me/zhixingye/im/ExampleUnitTest.java
@@ -0,0 +1,81 @@
+package me.zhixingye.im;
+
+import org.junit.Test;
+
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.ManagedChannel;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.okhttp.OkHttpChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void test() {
+//        OkHttpChannelBuilder builder = OkHttpChannelBuilder.forAddress("127.0.0.1", 5001).usePlaintext();
+//        ManagedChannel channel = builder.build();
+//        HelloRequest request = HelloRequest.newBuilder().setName("123").build();
+//        GreeterGrpc.GreeterBlockingStub blockingStub = GreeterGrpc.newBlockingStub(channel);
+//        HelloReply reply = blockingStub.sayHello(request);
+
+
+//        try {
+//            OkHttpChannelBuilder builder = OkHttpChannelBuilder.forAddress("127.0.0.1", 5001).usePlaintext().intercept(new ClientInterceptor() {
+//                @Override
+//                public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, Channel next) {
+//                    return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)){
+//                        @Override
+//                        public void start(ClientCall.Listener<RespT> responseListener, Metadata headers) {
+//                            super.start(responseListener, headers);
+//                        }
+//
+//                        @Override
+//                        public void sendMessage(ReqT message) {
+//                            super.sendMessage(message);
+//                        }
+//                    };
+//                }
+//            });
+//
+//            builder.compressorRegistry()
+//
+//            ManagedChannel channel = builder.build();
+//
+//            HelloRequest request = HelloRequest.newBuilder().setName("123").build();
+//            request.writeTo();
+//
+////            MetadataUtils.attachHeaders();
+//
+//            GreeterGrpc.GreeterStub futureStub = GreeterGrpc.newStub(channel).build(channel, CallOptions.DEFAULT);
+//            futureStub.sayHello(new StreamObserver<HelloReply>() {
+//                @Override
+//                public void onNext(HelloReply value) {
+//
+//                }
+//
+//                @Override
+//                public void onError(Throwable t) {
+//
+//                }
+//
+//                @Override
+//                public void onCompleted() {
+//
+//                }
+//            });
+//        } catch (Exception e) {
+//
+//        }
+    }
+}

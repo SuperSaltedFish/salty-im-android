86dad4398038e3ea3112e9324bb15427f8d8f7d0
zhixingye <zhixingye@tencent.com>
2020-01-09 19:58:41 +0800

    更新目录结构

--- im/im_core/src/test/java/me/zhixingye/im/ExampleUnitTest.java
+++ im/im_core/src/test/java/me/zhixingye/im/ExampleUnitTest.java
@@ -1,6 +1,13 @@
 package me.zhixingye.im;
 
+import com.google.protobuf.Any;
+import com.salty.protos.BasicReq;
+import com.salty.protos.BasicResp;
+import com.salty.protos.UserServiceGrpc;
+
 import org.junit.Test;
+
+import javax.annotation.Nullable;
 
 import io.grpc.CallOptions;
 import io.grpc.Channel;
@@ -11,10 +18,7 @@ import io.grpc.ManagedChannel;
 import io.grpc.Metadata;
 import io.grpc.MethodDescriptor;
 import io.grpc.okhttp.OkHttpChannelBuilder;
-import io.grpc.stub.StreamObserver;
-import me.zhixingye.im.grpc.GreeterGrpc;
-import me.zhixingye.im.grpc.HelloReply;
-import me.zhixingye.im.grpc.HelloRequest;
+import io.grpc.stub.ClientCallStreamObserver;
 
 
 public class ExampleUnitTest {
@@ -24,9 +28,10 @@ public class ExampleUnitTest {
             OkHttpChannelBuilder builder = OkHttpChannelBuilder.forAddress("127.0.0.1", 5001).usePlaintext().intercept(new ClientInterceptor() {
                 @Override
                 public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, Channel next) {
-                    return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)){
+                    return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
                         @Override
                         public void start(ClientCall.Listener<RespT> responseListener, Metadata headers) {
+
                             super.start(responseListener, headers);
                         }
 
@@ -38,29 +43,8 @@ public class ExampleUnitTest {
                 }
             });
 
-
             ManagedChannel channel = builder.build();
-
-            HelloRequest request = HelloRequest.newBuilder().setName("123").build();
-
-            GreeterGrpc.GreeterStub futureStub = GreeterGrpc.newStub(channel);
-            StreamObserver<HelloRequest> observer =    futureStub.sayHello(new StreamObserver<HelloReply>() {
-                @Override
-                public void onNext(HelloReply value) {
-
-                }
-
-                @Override
-                public void onError(Throwable t) {
-
-                }
-
-                @Override
-                public void onCompleted() {
-
-                }
-            });
-            observer.onNext(request);
+       
         } catch (Exception e) {
 
         }

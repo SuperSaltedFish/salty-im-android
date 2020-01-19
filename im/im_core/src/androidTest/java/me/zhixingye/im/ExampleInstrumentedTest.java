f272235b2af8b2d2fae99b26f17d31a0ce4fa42d
zhixingye <zhixingye@tencent.com>
2020-01-19 18:01:34 +0800

    编写单元测试

--- im/im_core/src/androidTest/java/me/zhixingye/im/ExampleInstrumentedTest.java
+++ im/im_core/src/androidTest/java/me/zhixingye/im/ExampleInstrumentedTest.java
@@ -1,10 +1,12 @@
 package me.zhixingye.im;
 
 import android.content.Context;
-import android.os.SystemClock;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.google.protobuf.MessageLite;
+import com.salty.protos.LoginResp;
 import com.salty.protos.RegisterResp;
 import com.salty.protos.SMSReq;
 import com.salty.protos.SMSResp;
@@ -13,7 +15,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Random;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CountDownLatch;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -33,7 +35,7 @@ public class ExampleInstrumentedTest {
 
     private static final String TAG = "ExampleInstrumentedTest";
 
-    private static Semaphore sSemaphore = new Semaphore(1);
+    private static CountDownLatch sLatch = new CountDownLatch(1);
     private static boolean isContinue = true;
 
     @Test
@@ -45,55 +47,117 @@ public class ExampleInstrumentedTest {
         Log.e(TAG, "测试结束");
     }
 
+
     private void startTest() {
         Random random = new Random();
         StringBuilder builder = new StringBuilder("1");
         for (int i = 0; i < 10; i++) {
             builder.append(random.nextInt(10));
         }
-        String account = builder.toString();
+        sAccount = builder.toString();
 
 
-//        testSMSReq(account);
-        testRegisterReq(account);
-
-        SystemClock.sleep(300000);
+        testSMSReq();
+        testRegisterReq();
+        testLoginReq();
     }
 
-    private void testSMSReq(String account) {
+    private static String sAccount;
+
+    private void testSMSReq() {
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder("1");
+        for (int i = 0; i < 10; i++) {
+            builder.append(random.nextInt(10));
+        }
+        sAccount = builder.toString();
         IMClient.get().getSMSService().obtainVerificationCodeForTelephoneType(
-                account,
+                sAccount,
                 SMSReq.CodeType.REGISTER,
-                new DefaultRequestCallback<SMSResp>());
+                new LockRequestCallback<>(new SimpleRequestCallback<SMSResp>() {
+                    @Override
+                    public void onCompleted(SMSResp response) {
+
+                    }
+                }));
     }
 
+    private String sPassword;
 
-    private void testRegisterReq(String account) {
+    private void testRegisterReq() {
+        sPassword = "yezhixing123";
         IMClient.get().getUserService().registerByTelephone(
-                account,
-                "yezhixing123",
+                sAccount,
+                sPassword,
                 "123456",
-                new DefaultRequestCallback<RegisterResp>());
+                new LockRequestCallback<>(new SimpleRequestCallback<RegisterResp>() {
+                    @Override
+                    public void onCompleted(RegisterResp response) {
+
+                    }
+                }));
     }
 
-    private static class DefaultRequestCallback<T extends MessageLite> implements RequestCallback<T> {
+    private void testLoginReq() {
+        IMClient.get().getUserService().loginByTelephone(
+                sAccount,
+                sPassword,
+                "",
+                new LockRequestCallback<LoginResp>(new SimpleRequestCallback<LoginResp>() {
+                    @Override
+                    public void onCompleted(LoginResp response) {
 
-        DefaultRequestCallback() {
-            sSemaphore.acquireUninterruptibly();
+                    }
+                }));
+    }
+
+    private static class LockRequestCallback<T extends MessageLite> implements RequestCallback<T> {
+
+        private static Handler mHandler = new Handler(Looper.getMainLooper());
+
+        private RequestCallback<T> mCallback;
+
+        LockRequestCallback(RequestCallback<T> callback) {
             assertTrue(isContinue);
+            mCallback = callback;
+            sLatch = new CountDownLatch(1);
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        sLatch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
 
         @Override
         public void onCompleted(T response) {
             Logger.e(TAG, response.toString());
-            sSemaphore.release();
+            if (mCallback != null) {
+                mCallback.onCompleted(response);
+            }
+            sLatch.countDown();
         }
 
         @Override
         public void onFailure(ErrorCode code) {
             Logger.e(TAG, code.toString());
             isContinue = false;
-            sSemaphore.release();
+            if (mCallback != null) {
+                mCallback.onFailure(code);
+            }
+            sLatch.countDown();
+
+        }
+    }
+
+    private static abstract class SimpleRequestCallback<T extends MessageLite> implements RequestCallback<T> {
+        @Override
+        public void onFailure(ErrorCode code) {
+
         }
     }
 }
--- im/im_core/src/main/java/me/zhixingye/im/service/UserService.java
+++ im/im_core/src/main/java/me/zhixingye/im/service/UserService.java
@@ -1,10 +1,13 @@
 package me.zhixingye.im.service;
 
+import com.salty.protos.LoginReq;
+import com.salty.protos.LoginResp;
 import com.salty.protos.RegisterReq;
 import com.salty.protos.RegisterResp;
 import com.salty.protos.UserProfile;
 import com.salty.protos.UserServiceGrpc;
 
+import androidx.annotation.Nullable;
 import me.zhixingye.im.listener.RequestCallback;
 import me.zhixingye.im.util.Sha256Util;
 
@@ -13,6 +16,9 @@ import me.zhixingye.im.util.Sha256Util;
  * 每一个不曾起舞的日子 都是对生命的辜负
  */
 public class UserService extends BasicService {
+
+    private static final String PASSWORD_SALTY = "salty";
+
     private UserServiceGrpc.UserServiceStub mUserServiceStub;
 
     public UserService() {
@@ -27,11 +33,22 @@ public class UserService extends BasicService {
 
         RegisterReq req = RegisterReq.newBuilder()
                 .setUserProfile(profile)
-                .setPassword(Sha256Util.sha256WithSalt(password, "salty"))
+                .setPassword(Sha256Util.sha256WithSalt(password, PASSWORD_SALTY))
                 .setVerificationCode(verificationCode)
                 .setRegisterType(RegisterReq.RegisterType.TELEPHONE)
                 .build();
 
         mUserServiceStub.register(createReq(req), new DefaultStreamObserver<>(RegisterResp.getDefaultInstance(), callback));
+    }
+
+    public void loginByTelephone(String telephone, String password, @Nullable String verificationCode, RequestCallback<LoginResp> callback) {
+        LoginReq req = LoginReq.newBuilder()
+                .setTelephone(telephone)
+                .setLoginType(LoginReq.LoginType.TELEPHONE)
+                .setPassword(Sha256Util.sha256WithSalt(password, PASSWORD_SALTY))
+                .setVerificationCode(verificationCode)
+                .build();
+
+        mUserServiceStub.login(createReq(req), new DefaultStreamObserver<>(LoginResp.getDefaultInstance(), callback));
     }
 }

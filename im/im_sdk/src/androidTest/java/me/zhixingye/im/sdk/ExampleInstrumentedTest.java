6d6395f86773b47ffe93bb7f52145eb94f3fe361
zhixingye <zhixingye@tencent.com>
2020-02-25 23:02:12 +0800

    更新目录结构

--- im/im_sdk/src/androidTest/java/me/zhixingye/im/sdk/ExampleInstrumentedTest.java
+++ im/im_sdk/src/androidTest/java/me/zhixingye/im/sdk/ExampleInstrumentedTest.java
@@ -19,8 +19,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-   、
-
+        
     }
 }
